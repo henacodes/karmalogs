fafb9f69b85418178c935fa699309f030370a3d4
kirakos <henacodes@gmail.com>
2025-05-11 21:36:38 +0300

    updated bot handle

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const INVALID_RATING_FORMAT =
   "Invalid format. Use: @BotName <rating 1-10> [optional comment]";
-export const BOT_USERNAME = "KiraDevTestBot";
+export const BOT_USERNAME = "KarmalogsBot";
 export const MONGO_URL = Deno.env.get("MONGO_DB_URL");
